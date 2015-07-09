645b41abeca5e4c0765da46dc2581cc8a20d84fc
Jlortegah <jlortegah@gmail.com>
2015-07-09 11:33:10 +0200

    Create plot1.R
    R script to create plot1

--- plot1.R
+++ plot1.R
@@ -11,3 +11,4 @@ globalActivePower <- as.numeric(subSetDataByDate$Global_active_power)
 png("plot1.png", width=480, height=480)
 hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
+
